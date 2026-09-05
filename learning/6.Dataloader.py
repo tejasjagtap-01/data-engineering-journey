966ae205daea94f2ea4325a210b6b3fc564374f8
tejasjagtap-01 <tejasjagtap9977@gmail.com>
2026-09-06 01:18:31 +0530

    Rethinking Push -6

--- learning/6.Dataloader.py
+++ learning/6.Dataloader.py
@@ -11,13 +11,13 @@ class DataLoader:
         print("Data loaded Successfully")
         return self.data 
 
-# race_results = DataLoader(r"C:\Users\tejas\OneDrive\Desktop\numpy python\11. F1\f1\fact_race_results.csv")
-# race_results = race_results.load()
-# print(race_results.head())
+race_results = DataLoader(r"C:\Users\tejas\OneDrive\Desktop\numpy python\11. F1\f1\fact_race_results.csv")
+race_results = race_results.load()
+print(race_results.head())
 
-# drivers =  DataLoader("C:/Users/tejas/OneDrive/Desktop/numpy python/11. F1/drivers.csv")
-# drivers = drivers.load()
-# print(drivers.tail())
+drivers =  DataLoader("C:/Users/tejas/OneDrive/Desktop/numpy python/11. F1/drivers.csv")
+drivers = drivers.load()
+print(drivers.tail())
 
 fact_lap_times = DataLoader("C:/Users/tejas/OneDrive/Desktop/numpy python/11. F1/f1/fact_lap_times.csv")
 fact_lap_times = fact_lap_times.load()
